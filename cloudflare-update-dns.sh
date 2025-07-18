c93e98794b28e9ea991aa8f65c3fa9c3bcab8921
Matthew Wolff <matthewjwolff@gmail.com>
2025-07-17 22:10:26 -0500

    move to external file

--- cloudflare-update-dns.sh
+++ cloudflare-update-dns.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-DIR="$(dirname "$0")"
-source "$DIR/config.sh"
+source "/etc/cloudflare_dns.sh"
+
+CHALLENGE_PREFIX="_acme-challenge"
+CHALLENGE_DOMAIN="${CHALLENGE_PREFIX}.${CERTBOT_DOMAIN}"
+PARENT_DOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< "${CERTBOT_DOMAIN}")
+
+CLOUDFLARE_ZONE=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${PARENT_DOMAIN}" \
+     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+     -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
+     -H "Content-Type: application/json" -s | jq -r '.result[0].id')
 
 DNS_SERVER=8.8.8.8
 
